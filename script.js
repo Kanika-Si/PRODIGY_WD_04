aa05953e17fa84c9d886f9e0feaf3a624f043b5e
Kanika-Si <156156203+Kanika-Si@users.noreply.github.com>
2024-07-10 08:52:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.main-nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
